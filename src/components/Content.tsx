a68024a68d5a1fd79c3be4a5b69a5ce937416b23
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-01-14 20:52:13 +0100

    fixed comp's aspect ratio for mobile

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,21 +5,20 @@ import styled from 'styled-components';
 import { FAQ, Illustration } from '../components';
 
 const ContentWrapper = styled.div`
-background: ${props => props.theme.colors.contentBg};
-position: relative;
-width: 100%;
-margin: 0 ${props => props.theme.margins.sm};
-margin-top: calc(63vw * 0.4);
-height: 67vh;
-border-radius: 1.64rem;
 display: flex;
 flex-wrap: wrap;
 align-items: center;
 justify-content: space-between;
+position: relative;
+width: calc(100vw - 48px);
+height: calc((100vw - 48px) * 1.63);
+margin: 0 24px;
+border-radius: 23px;
+background: ${props => props.theme.colors.contentBg};
 
-@media screen and (min-width: 436px) {
-    width: calc(436px - 24px * 2);
-    margin-top: calc(436px * 0.63 * 0.4);
+@media screen and (min-width: 400px) {
+    width: calc(400px - 48px);
+    height: calc((400px - 48px) * 1.63);
     margin-left: auto;
     margin-right: auto;
 }
